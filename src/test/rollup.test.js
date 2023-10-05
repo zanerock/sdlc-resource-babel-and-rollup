fac2633805ff71a9aeae277137612517bc33d703
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-05 12:15:55 -0500

    implemented configurable "license header" options and tested

--- src/test/rollup.test.js
+++ src/test/rollup.test.js
@@ -1,5 +1,9 @@
+// TODO: I tried to implement this with jest, but couldn't get it to work. Not clear why. Did have to enable module 
+// support, but that seemed insufficient. We might consider rewriting 'rollup.config.mjs' as cjs and trying agaian.
+
 const loadConfigFile = require('rollup/dist/loadConfigFile.js').loadConfigFile // eslint-disable-line import/extensions
-const path = require('path')
+const { readFileSync } = require('node:fs')
+const path = require('node:path')
 const rollup = require('rollup')
 
 const configFile = path.resolve(__dirname, '..', '..', 'dist', 'rollup', 'rollup.config.mjs')
@@ -8,7 +12,7 @@ const testInput = path.resolve(__dirname, 'throw-expression.mjs')
 
 const testStagingPath = path.resolve(__dirname, '..', '..', 'test-staging')
 
-const testOutputPath = path.resolve(testStagingPath, 'test-output.js')
+const testOutputPath = path.resolve(testStagingPath, 'test-output.cjs')
 
 const testOutput = {
   file          : testOutputPath,
@@ -78,6 +82,15 @@ loadConfigFile(configFile, { input : testInput, output : testOutput })
       catch (e) {
         process.stdout.write('FAILED (unexpected throw: ' + e.message + ')')
         process.exit(1)
+      }
+      // TEST 4
+      process.stdout.write("appends 'src/file-header.txt': ")
+      const testOutputContents = readFileSync(testOutputPath, { encoding: 'utf8' })
+      if (testOutputContents.match(/Copyright 2023/m)) {
+        process.stdout.write('PASSED\n')
+      }
+      else {
+        process.stdout.write('FAILED\n')
       }
     })
   })
