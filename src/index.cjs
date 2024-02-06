9eee0707a5fae1880e52a4bfdc2095da1e3973de
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-06 13:08:03 -0600

    Export babel config from package

---
+++ src/index.cjs
@@ -0,0 +1,5 @@
+const babelConfig = require('./babel/babel.config.cjs')
+// const rollupConfig = require('./rollup.config.mjs')
+
+exports.babelConfig = babelConfig
+// exports.rollupConfig = rollupConfig
